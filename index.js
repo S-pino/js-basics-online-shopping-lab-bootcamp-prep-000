a60ffefa92598ad5a021bb84ba5fe4b105d1a585
Stephanie Pino <stephy_pino@hotmail.com>
2020-09-15 23:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function setCart(c) {
 
 function addToCart(item) {
 var price = Math.floor(Math.random()*100);
+if (cart.length === 1) {
+  return 
+}
 cart.push({[item]: price});
 console.log(`${item} has been added to your cart.`);
 return cart;

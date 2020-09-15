3d52a8347c74f8561ac78600b0f784edb5b90ad8
Stephanie Pino <stephy_pino@hotmail.com>
2020-09-15 23:23:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-
+cart.push({[item]: Math.floor(Math.random()*100)});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+}
 
 console.log(`${item} has been added to your cart.`);
 return cart;
